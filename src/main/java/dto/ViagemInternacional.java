f03da5a6de9b0423ef42b2d6bda5a8f8acd40fc7
ALEXANDRE LIBERAL DE SIQUEIRA <alexliber@gmail.com>
2023-03-17 11:16:14 -0300

    Initial commit Primeiro curso de API

---
+++ src/main/java/dto/ViagemInternacional.java
@@ -0,0 +1,43 @@
+package dto;
+
+import enums.Destinos;
+import interfaces.CalculadoraDePrevisao;
+import utils.ArquivosUtils;
+import java.util.List;
+
+
+public class ViagemInternacional extends Viagem implements CalculadoraDePrevisao {
+    private String passaporte;
+
+    public ViagemInternacional(Destinos lugarDeDestinos) {
+        super(lugarDeDestinos);
+    }
+
+    public String getPassaporte() {
+        return this.passaporte;
+    }
+
+    public void setPassaporte(String passaporte){
+        this.passaporte = passaporte;
+    }
+
+    @Override
+    public void setAcompanhantes(List<Acompanhante> acompanhantes) throws Exception {
+
+       int limiteDeAcompanhantes = Integer.parseInt(ArquivosUtils.getPropriedade("viagem.internacional.acompanhantes.limite"));
+
+        if (acompanhantes.size() <= limiteDeAcompanhantes) {
+            super.setAcompanhantes(acompanhantes);
+        } else {
+            throw new Exception("Viagens internacionais não podem ter mais que " + limiteDeAcompanhantes + " acompanhante");
+        }
+    }
+
+    public int calcularPrevisaoDeDiasParaRetorno(){
+        if (this.getDestino().equals(Destinos.MIAMI)) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
